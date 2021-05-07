1fb4047c71027e46cfc642dfb17092c66169da99
David Lee <djwodus@gmail.com>
2021-05-07 13:45:54 +0900

    more secure and asynchronous firebase

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 import AppRouter from 'components/Router';
 import { authService } from "fbase";
 
+function unsafeAuth() {
+  let nullableAuth = null;
+  authService().then((auth) => nullableAuth = auth);
+  return nullableAuth;
+}
+
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+    const [isLoggedIn, setIsLoggedIn] = useState(unsafeAuth().currentUser);
     return (
         <>
           <AppRouter isLoggedIn={isLoggedIn} />
--- src/fbase.js
+++ src/fbase.js
@@ -1,16 +1,10 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
-
-  firebase.initializeApp(firebaseConfig);
-
-  export const authService = firebase.auth();
+  export const authService = async function () {
+    if(firebase.apps?.length > 0) return firebase.auth();
+    else {
+      const res = await fetch('/__/firebase/init.json');
+      return firebase.initializeApp(await res.json()).auth();
+    }
+  }
