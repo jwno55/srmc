6647f89f758502416640573f945f80bec3b729a2
jwno55 <jwno55@kidkids.net>
2021-06-23 09:19:22 +0900

    이미지 업로드 기능 추가

--- src/routes/BookApplication.js
+++ src/routes/BookApplication.js
@@ -1,22 +1,56 @@
-import React, { useState } from "react";
-import { dbService } from "fbase";
+import React, { useState, useEffect } from "react";
+import { dbService, storageService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
 
 const BookApplication = () => {
   const [books, setBooks] = useState("");
+  const [attachment, setAttachment] = useState();
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("bookapplication").add({
-      books,
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const attachmentRef = storageService
+        .ref()
+        .child(`${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const bookObj = {
+      text: books,
       createdAt: Date.now(),
-    });
+      attachmentUrl,
+    };
+    await dbService.collection("bookapplication").add(bookObj);
     setBooks("");
+    setAttachment("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setBooks(value);
   };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      //console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment(null);
+  
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -27,7 +61,14 @@ const BookApplication = () => {
           placeholder="Book Name"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Application" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
     </div>
   );
